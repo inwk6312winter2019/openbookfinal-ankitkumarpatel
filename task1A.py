580a830f3e87c9e18d7e7ab7c253e28b99f8c9e0
ankitkumarpatel <ankitkumar.patel@dal.ca>
2019-02-15 10:01:33 -0800

    first subrtask done

--- task1A.py
+++ task1A.py
@@ -1,1 +1,45 @@
-#
+
+
+import string
+
+
+def unique_words(filename):
+
+    hist = histogram(filename)
+    mylist = list(hist)
+    mylist =  list(dict.fromkeys(mylist))
+
+    return mylist
+
+
+
+def count_the_article(0):
+
+
+
+
+def sorted_words(0):
+
+
+
+
+
+def character_word_count(0):
+
+
+def starts_with_vow():
+
+
+
+
+def histogram(filename):
+
+    hist =dict()
+    file = open('filename')
+    for line in file:
+        line = line.split()
+        for word in line:
+            word = word.strip(string.punctuation + string.whitespace).lower()
+            hist[word] = hist.get(word, 0) + 1
+
+    return hist
